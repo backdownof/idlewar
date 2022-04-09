2c2833658de6e263fe552fd170ee3cd25325cab4
backdownof <bogdanovan88@gmail.com>
2022-04-09 22:08:12 +0300

    switched main file

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from asyncore import dispatcher
 from config import TELE_TOKEN
 
 from telegram.ext import Updater
 
-updater = Updater(token=TELE_TOKEN, use_context=True)
+from context import start_handler, echo_handler, unknown_handler
+
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+updater = Updater(token=TELE_TOKEN)
+dispatcher = updater.dispatcher
+
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(echo_handler)
+dispatcher.add_handler(unknown_handler)
+
+
+updater.start_polling()
